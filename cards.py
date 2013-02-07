a3788c6f03fce508cb2e4f2b17007678f52bcc61
Gerry Salinas <gerry.salinas@gmail.com>
2013-02-07 00:23:55 -0800

    Added drawing cards from the deck.

--- cards.py
+++ cards.py
@@ -3,59 +3,84 @@
 import random
 
 class Card(object):
-	"""Represents a Dungeon Petz card.
+    """Represents a Dungeon Petz card.
 
-	An instance of this class is a single card from one of the Need decks.
-	A card has attributes for its color, its primary symbol, and its
-	secondary symbol if any."""
+    An instance of this class is a single card from one of the Need decks.
+    A card has attributes for its color, its primary symbol, and its
+    secondary symbol if any."""
 
-	# Also contains whatever I decide I need to make advanced cards work.
+    # Also contains whatever I decide I need to make advanced cards work.
 
-	def __init__(self, color, symbol, secondary_symbol=None):
-		"""Initialize the card.
-		
-		Expects a string for card color, a string for card symbol, and
-		an optional string for a secondary card symbol."""
+    def __init__(self, color, symbol, secondary_symbol=None):
+        """Initialize the card.
+        
+        Expects a string for card color, a string for card symbol, and
+        an optional string for a secondary card symbol."""
 
-		self.color = color
-		self.symbol = symbol
-		
-		if secondary_symbol != None:
-			self.has_secondary = True
-			self.secondary_symbol = secondary_symbol
-		else:
-			self.has_secondary = False
+        self.color = color
+        self.symbol = symbol
+        
+        if secondary_symbol != None:
+            self.has_secondary = True
+            self.secondary_symbol = secondary_symbol
+        else:
+            self.has_secondary = False
+    
+    def __str__(self):
+            """Define the string representation of a Card object."""
+
+            return self.color + " " + self.symbol
 
 class Deck(object):
-	"""Represents a Needs deck of a particular color.
+    """Represents a Needs deck of a particular color.
 
-	An instance of this class is a single deck of a particular color. Other
-	parts of the code can draw cards from a deck and discard cards to a
-	deck, and it will automatically shuffle itself as needed."""
+    An instance of this class is a single deck of a particular color. Other
+    parts of the code can draw cards from a deck and discard cards to a
+    deck, and it will automatically shuffle itself as needed."""
 
-	def __init__(self, color, distribution):
-		"""Initialize the deck.
-		
-		Expects a string for the deck color and a list of string /
-		number pairs for the symbol distribution."""
+    def __init__(self, color, distribution):
+        """Initialize the deck.
+        
+        Expects a string for the deck color and a list of string /
+        number pairs for the symbol distribution."""
 
-		self.color = color
-		self.draw_pile = []
-		self.discard_pile = []
+        self.color = color
+        self.draw_pile = []
+        self.discard_pile = []
 
-		for x in distribution:
-			symbol = x[0]
-			number = x[1]
+        for x in distribution:
+            symbol = x[0]
+            number = x[1]
 
-			for i in range(number):
-				new_card = Card(self.color, symbol)
-				self.draw_pile.append(new_card)
+            for i in range(number):
+                new_card = Card(self.color, symbol)
+                self.draw_pile.append(new_card)
 
-		self.shuffleDeck()
+        self.shuffleDeck()
 
-	def shuffleDeck(self):
-		"""Add the discard pile back to the deck and reshuffle."""
-		self.draw_pile.extend(self.discard_pile)
-		self.discard_pile[:] = []
-		random.shuffle(self.draw_pile)
+    def shuffleDeck(self):
+        """Add the discard pile back to the deck and reshuffle."""
+        self.draw_pile.extend(self.discard_pile)
+        self.discard_pile[:] = []
+        random.shuffle(self.draw_pile)
 
+    def drawSingleCard(self):
+        """Return a card from the deck, reshuffling if needed.
+        
+        This is a helper function that should not be called externally, which
+        returns a single card. External calls should use drawCard, which calls
+        this function as many times as needed."""
+
+        if not self.draw_pile:
+            self.shuffleDeck()
+
+        return self.draw_pile.pop(0)
+
+    def drawCard(self, num=1):
+        """Return num cards from the deck."""
+        return_list = []
+        
+        for i in range(num):
+            return_list.append(self.drawSingleCard())
+
+        return return_list
--- pets.py
+++ pets.py
@@ -9,68 +9,68 @@ class Pet(object):
     etc., return a list of cards a player must draw. """
 
     def __init__(self, name, needs_list, diet, age=2):
-    	"""Initialize the pet.
-	
-	Expects a string name, a list of needs,	full-length and in order with
-	respect to the age of the pet. Diet should be a string - omnivore,
-	carnivore, or herbivore. Age should be	an integer between 2 and the
-	length of the needs & price lists. Defaults to age 2 if age is omitted.
-	Defaults to standard pricing and aging, but if these are set to other
-	values nothing should break."""
+        """Initialize the pet.
+    
+        Expects a string name, a list of needs, full-length and in order with
+        respect to the age of the pet. Diet should be a string - omnivore,
+        carnivore, or herbivore. Age should be  an integer between 2 and the
+        length of the needs & price lists. Defaults to age 2 if age is omitted.
+        Defaults to standard pricing and aging, but if these are set to other
+        values nothing should break."""
         
-	# Default pricing based on pet diet.
+        # Default pricing based on pet diet.
         carnivore_pricing = [0,0,0,3,4,5,6]
-	herbivore_pricing = [0,0,0,2,3,4,5]
-	omnivore_pricing = [0,0,0,1,2,3,4]
-	
-	# Default pet aging.
-	self.aging_chart = [0,0,2,2,1,1,1,0]
+        herbivore_pricing = [0,0,0,2,3,4,5]
+        omnivore_pricing = [0,0,0,1,2,3,4]
+    
+        # Default pet aging.
+        self.aging_chart = [0,0,2,2,1,1,1,0]
 
         # Assign basic attributes.
-    	self.name = name
-	self.needs_list = needs_list
-	self.age = age
-	self.misery = 0
-	self.mutations = 0
+        self.name = name
+        self.needs_list = needs_list
+        self.age = age
+        self.misery = 0
+        self.mutations = 0
 
         # Create a diet dictionary.
-	diet_cases = {
-			'Omnivore': [True, True, omnivore_pricing],
-			'Carnivore': [True, False, carnivore_pricing],
-			'Herbivore': [False, True, herbivore_pricing],
-	}
+        diet_cases = {
+            'Omnivore': [True, True, omnivore_pricing],
+            'Carnivore': [True, False, carnivore_pricing],
+            'Herbivore': [False, True, herbivore_pricing],
+        }
 
         # Set carnivore and herbivore flags.
-	[self.carnivore, self.herbivore] = diet_cases.get(diet)
+        [self.carnivore, self.herbivore] = diet_cases.get(diet)
 
-        # Set pricing based on diet defaults.
-	self.pricing_chart = diet_cases.get(diet)[-1]
+        # Set pricing based on diet defaults. Gets the last dictionary item
+        self.pricing_chart = diet_cases.get(diet)[-1]
 
     def growUp(self, i=None):
-    	"""Age the pet according to its growth chart or the passed argument.
-	
-	If no arguments are given, ages the pet according to its growth chart.
-	If an argument is given, ages the pet that many steps instead."""
+        """Age the pet according to its growth chart or the passed argument.
+    
+        If no arguments are given, ages the pet according to its growth chart.
+        If an argument is given, ages the pet that many steps instead."""
 
-	if i = None:
-		i = self.aging_chart[self.age]
-	
-	self.age += i
+        if i = None:
+            i = self.aging_chart[self.age]
+    
+        self.age += i
 
     def currentNeeds(self):
-    	"""Return a list of card types this pet needs based on its age."""
-    	
-	return self.needs_list[:self.age]
+        """Return a list of card types this pet needs based on its age."""
+        
+        return self.needs_list[:self.age]
 
     def checkStatus(self):
-    	"""Check if the pet dies due to mutation or misery."""
-	goes_away = False
+        """Check if the pet dies due to mutation or misery."""
+        goes_away = False
 
-	if self.mutations > 1:
-		goes_away = True
-	
-	if self.misery > self.age:
-		goes_away = True
-	
-	return goes_away
+        if self.mutations > 1:
+            goes_away = True
+    
+        if self.misery > self.age:
+            goes_away = True
+    
+        return goes_away
 
